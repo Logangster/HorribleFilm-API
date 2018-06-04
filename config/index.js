06a09c8d2f2157023520912650fc2043f6d45855
Logan Gore <rockstar171@gmail.com>
2018-06-03 20:37:59 -0600

    Accidentally switched routes around

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json');
 const protectedRoutes = require('./protected-routes');
-const publicRoutes = require('./protected-routes');
+const publicRoutes = require('./public-routes');
 
 module.exports = {
   config,
